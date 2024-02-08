e8d34432a40fd45cc829dd9acd0c02246e3c9516
choudharysneha1708@gmail.com <iit2021132@iiita.ac.in>
2024-02-08 12:48:55 +0530

    some basics of js

---
+++ test.js
@@ -0,0 +1,14 @@
+let name = "sneha";
+var state = "Bihar";
+// data type
+/*
+    number=> 2 ^53
+    bigint
+    string => ""
+    boolena => true/false
+    null => standalone value
+    underfine =>
+    symbol => unique
+    object
+ */
+console.log(typeof name)
